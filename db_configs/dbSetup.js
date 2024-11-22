d93a9abf855d1c2f9044585a1a44d123ba2e3a84
kavinkumars21 <kavinkumars218@gmail.com>
2024-11-22 13:44:03 +0530

    solved db issue 3

--- db_configs/dbSetup.js
+++ db_configs/dbSetup.js
@@ -1,44 +1,37 @@
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabase('chatApp.db');
+const db = SQLite.openDatabaseAsync('chatApp.db');
 
-export const setupDatabase = () => {
-  db.transaction(tx => {
-    tx.executeSql('PRAGMA journal_mode = WAL;');
+export const setupDatabase = async () => {
+  try {
+    await db.execAsync(`PRAGMA journal_mode = WAL;`);
 
-    tx.executeSql(
+    await db.runAsync(
       `CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         userId TEXT,
         message TEXT,
         timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-        delivered BOOLEAN NOT NULL CHECK (delivered IN (0, 1))
-      );`,
-      [],
-      () => console.log('Messages table created successfully'),
-      (_, error) => {
-        console.error('Error creating messages table:', error);
-        return true;
-      }
+        delivered BOOLEAN
+      );`
     );
+    console.log('Messages table created successfully');
 
-    tx.executeSql(
+    await db.runAsync(
       `CREATE TABLE IF NOT EXISTS friends (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId TEXT NOT NULL UNIQUE,
+        userId TEXT NOT NULL,
         name TEXT NOT NULL,
-        profilePicture TEXT,
+        profilePicture TEXT NOT NULL,
         email TEXT NOT NULL,
-        phoneNumber TEXT NOT NULL
-      );`,
-      [],
-      () => console.log('Friends table created successfully'),
-      (_, error) => {
-        console.error('Error creating friends table:', error);
-        return true;
-      }
+        phoneNumber TEXT NOT NULL,
+        UNIQUE(userId)
+      );`
     );
-  });
+    console.log('Friends table created successfully');
+  } catch (error) {
+    console.error('Error setting up database', error);
+  }
 };
 
 export default db;
